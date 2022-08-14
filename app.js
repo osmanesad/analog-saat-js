d7cf7ad00bffc3e84c56495e40790b67a33bd296
G-0324D <byesad@gmail.com>
2022-08-14 05:41:57 +0300

    Update - Digital clock added
    Analog saatin altına dijital göstergeli saat ekranı eklendi.

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
+const secondTime = document.getElementById("second");
 
-const secondTime = document.getElementById("second")
+const minuteTime = document.getElementById("minute");
 
-const minuteTime = document.getElementById("minute")
+const hourTime = document.getElementById("hour");
 
-const hourTime = document.getElementById("hour")
 
-function getTime(){
-    const now = new Date()
-    const second = now.getSeconds()
-    const minute = now.getMinutes()
-    const hour = now.getHours()
-    const timeInterval = 6
 
-    secondTime.style.transform = "rotate(" + (second * timeInterval) + "deg)"
-    minuteTime.style.transform = "rotate(" + (minute * timeInterval + second /10) + "deg)"
-    hourTime.style.transform = "rotate(" + (hour * 30 + minute / 2) + "deg)"
+function getTime() {
+  const now = new Date();
+  const second = now.getSeconds();
+  const minute = now.getMinutes();
+  const hour = now.getHours();
+  const timeInterval = 6;
 
-    console.log(second)
-    console.log(second * timeInterval)
-    console.log(minute * timeInterval + second / 10)
-    console.log(hour * 30 + minute / 2)
+  secondTime.style.transform = "rotate(" + second * timeInterval + "deg)";
+  minuteTime.style.transform =
+    "rotate(" + (minute * timeInterval + second / 10) + "deg)";
+  hourTime.style.transform = "rotate(" + (hour * 30 + minute / 2) + "deg)";
+
+  // console.log(second)
+  //console.log(second * timeInterval)
+  //console.log(minute * timeInterval + second / 10)
+  //console.log(hour * 30 + minute / 2)
+  console.log(hour)
+  document.getElementById('time').innerHTML = hour + ":" + minute + ":" + second;
 }
+
 
 setInterval(getTime, 100)
 
@@ -28,12 +33,10 @@ setInterval(getTime, 100)
 
 
 
+/*
+for(var x=0; x <= 12; x++){
+    document.writeln(x)
+    document.getElementById('number').innerHTML = x;
 
-
-
-
-
-
-
-
-
+}
+*/
